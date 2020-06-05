904a67f26fd23dfd88605e35abd4f96d80c46372
Hemanth Kumar <56812697+kumar342@users.noreply.github.com>
2020-06-05 15:59:35 +0530

    bootstrap style  added in public

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 export default class Header extends Component {
+  state = {
+    user: true,
+  };
+
+  componentDidMount = () => {
+    console.log(localStorage.getItem("user"));
+
+    this.setState({ user: localStorage.getItem("user") });
+  };
+
+  logOut = () => {
+    this.setState({ user: null });
+  };
   render() {
-    return <div> Header added</div>;
+    if (!this.state.user) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <nav className="navbar navbar-light bg-dark">
+        <button type="button" className="btn btn-primary">
+          welcome {localStorage.getItem("user")}
+        </button>
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <button onClick={this.logOut}>Sign out!</button>
+          </li>
+          <li></li>
+        </ul>
+      </nav>
+    );
   }
 }
